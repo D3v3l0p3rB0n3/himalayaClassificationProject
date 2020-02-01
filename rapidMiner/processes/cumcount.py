95241740331413167fde454e8e1657af67cfcda4
sonnyplankton <57801822+sonnyplankton@users.noreply.github.com>
2020-02-01 14:21:54 +0100

    refinments on python script

--- rapidMiner/processes/cumcount.py
+++ rapidMiner/processes/cumcount.py
@@ -1,6 +1,10 @@
 def rm_main(data):
     #receive data set as data
 
+   #Sort data by year of memberclimb to ensure eperience is calculated in correct order
+    data.sort_values("myear", axis = 0, ascending = True, 
+                 inplace = True, na_position ='last')
+    
     #create new colum showing the experience of a climber by counting the apperance of the unique identifier
     #from a combination of lastname and fristname
     data["climber_experience"] = data.groupby([data["lname_fname"]]).cumcount()+1
